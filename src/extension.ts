df09583e69eeac5a73200b4675030941969f531c
raibbl <raibbl1997@gmail.com>
2024-12-16 18:23:07 -0600

    typescriptlens  precentage command

--- src/extension.ts
+++ src/extension.ts
@@ -26,7 +26,7 @@ export function activate(context: vscode.ExtensionContext) {
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
   const disposable = vscode.commands.registerCommand(
-    "typescriptlens.helloWorld",
+    "typescriptlens.precentage",
     async () => {
       // The code you place here will be executed every time your command is executed
       const percentage = await getTypeScriptPercentage();
